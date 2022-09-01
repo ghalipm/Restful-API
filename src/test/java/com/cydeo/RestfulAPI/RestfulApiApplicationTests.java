6cfb94b2598eb2e7935c89cfb14c644231212cb5
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-08-31 23:35:58 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/RestfulAPI/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.RestfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
